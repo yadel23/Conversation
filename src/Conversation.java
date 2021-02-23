10e32f2d26895d8dc367b381af51d92132850538
a-r-t <alexthimineur@gmail.com>
2021-02-23 18:56:26 -0500

    case insensitive on if statemetn for checking if answer is no

--- src/Conversation.java
+++ src/Conversation.java
@@ -9,7 +9,7 @@ public class Conversation {
 		System.out.println("Nice to meet you " + name + "!");
 		System.out.println("Do you like online school?");
 		String answer = keyboardInput.nextLine();
-		if (answer.equals("No")) {
+		if (answer.equalsIgnoreCase("no")) {
 			System.out.println("Same! I miss my podium. :(");
 		} else {
 			System.out.println("I do not miss parking in North Lot.");
